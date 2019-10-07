b3bb1d8e63a9ee6765e78d886661f0c60752eedb
Arturo Filastò <arturo@filasto.net>
2019-10-07 19:31:28 +0200

    Fix mass-label

--- mass-label.sh
+++ mass-label.sh
@@ -1,6 +1,18 @@
+#!/bin/bash
+set -ex
+
 # hub api /orgs/ooni/repos | jq -r '.[] | .name' > repos.txt
+echo "Synching labels with repo"
 for repo in $(cat repos.txt | grep -v '^#');
 do
     labeler scan labels-${repo}.yaml --repo ooni/${repo}
-    #labeler apply labels-${repo}.yaml --repo ooni/${repo}
+done
+
+echo "Merging with base labels"
+python merge-labels.py
+
+echo "Writing changes to remote"
+for repo in $(cat repos.txt | grep -v '^#');
+do
+    labeler apply labels-${repo}.yaml --repo ooni/${repo}
 done
