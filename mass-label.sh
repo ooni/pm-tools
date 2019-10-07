9dc451e276350551630d801d5886691461672f8b
Arturo Filastò <arturo@filasto.net>
2019-10-07 16:10:21 +0200

    Snapshot of the current labels

--- mass-label.sh
+++ mass-label.sh
@@ -1,5 +1,5 @@
 # hub api /orgs/ooni/repos | jq -r '.[] | .name' > repos.txt
-for repo in $(cat repos.txt);
+for repo in $(cat repos.txt | grep -v '^#');
 do
     labeler scan labels-${repo}.yaml --repo ooni/${repo}
 done
