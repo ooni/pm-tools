b2802aaf9e1c2236ffd0cf3de03f9940c6797c0e
Arturo Filastò <arturo@filasto.net>
2019-11-20 11:23:36 +0100

    Add basic readme, zap dead code

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# OONI PM Tools
+
+This repository includes code which is useful to project management.
+
+The labels inside of this repository also consistute the base template labels
+common to all OONI repositories.
+
+To sync the labels with the ones in this repo run: `python sync-labels.py`.
+
+Other useful scripts in here include `add-to-teams.py`, which adds issues to
+the relevant github project board in the icebox column.
--- sync-labels.py
+++ sync-labels.py
@@ -12,7 +12,6 @@ def load_base_labels():
     return base_labels
 
 def sync_labels(repo, base_labels):
-    repo = g.get_organization('ooni').get_repo(repo)
     for label in repo.get_labels():
         base_label = base_labels.pop(label.name.lower(), None)
         if base_label is not None:
@@ -20,11 +19,17 @@ def sync_labels(repo, base_labels):
             label.edit(
                     name=base_label.name,
                     color=base_label.color,
-                    description=base_label.description
+                    description=base_label.description or ''
             )
     for label_name, label in base_labels.items():
         print('adding label {}'.format(label.name))
         repo.create_label(name=label.name, color=label.color, description=label.description or '')
 
-base_labels = load_base_labels()
-sync_labels(repo='github-webhooks', base_labels=base_labels)
+def main():
+    base_labels = load_base_labels()
+    for repo in g.get_organization('ooni').get_repos():
+        print('Synching labels in ooni/{}'.format(repo.name))
+        sync_labels(repo=repo, base_labels=base_labels)
+
+if __name__ == '__main__':
+    main()
